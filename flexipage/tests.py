b74bf74e0df78545ab816f8f99639a9ccf3c7d3d
mrmagooey <peter.davis8@gmail.com>
2013-01-06 14:54:37 +1100

    Started tests.py

--- flexipage/tests.py
+++ flexipage/tests.py
@@ -9,8 +9,30 @@ from django.test import TestCase
 
 
 class SimpleTest(TestCase):
+    def test_templates_identified(self):
+        pass
+        
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+        
+class UtilsTest(TestCase):
+    def test_get_flexi_template(self):
+        pass
+        
+class FlexiPageTest(TestCase):
+    
+    
+    def test_templates_available(self):
+        pass
+
+class FormsTest(TestCase):
+    def test_modelforms_render(self):
+        pass
+
+    def test_modelforms_save(self):
+        pass
+        

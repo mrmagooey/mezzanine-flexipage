ad97169780c05bc987730fe6654a89b205e159df
mrmagooey <peter.davis8@gmail.com>
2013-03-29 20:11:56 +1100

    Updated version number in setup.py to reflect point release status

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mezzanine-flexipage',
-      version='0.2alpha',
+      version='0.1.1',
       description='',
       url='http://github.com/mrmagooey/flexipage',
       author='Peter Davis',

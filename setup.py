54e773e76a4decd4ef8a9bde1e1bad24be6bca5c
mrmagooey <peter.davis8@gmail.com>
2013-04-03 20:57:40 +1100

    Remove duplicated {% editable_loader %} tag in start.html to stop "an error occurred" errors, update setup.py to reflect version change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mezzanine-flexipage',
-      version='0.1.1',
+      version='0.1.2',
       description='',
       url='http://github.com/mrmagooey/flexipage',
       author='Peter Davis',
